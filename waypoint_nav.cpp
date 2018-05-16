de305869c66d97d67989f73dbd03c1abf990221d
AcharyaAditya <fabritya@gmail.com>
2018-05-16 13:57:07 -0700

    Initial Commit

---
+++ waypoint_nav.cpp
@@ -0,0 +1,352 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"   //for command velocity
+#include "geometry_msgs/Vector3.h" //for command velocity
+#include "std_msgs/Empty.h"		   //For take off and landing
+#include "sensor_msgs/Imu.h"
+#include "nav_msgs/Odometry.h"
+
+#include "ardrone_autonomy/CamSelect.h" // For toggling camera
+#include "ardrone_autonomy/Navdata.h"   //Accessing ardrone's published data
+#include "ardrone_autonomy/vector31.h"
+#include "ardrone_autonomy/vector21.h"
+
+#include <sensor_msgs/Joy.h>
+#include <stdio.h>
+#include <math.h>
+#include <termios.h>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include "std_msgs/String.h"
+#include "std_msgs/Float64.h"
+
+// class variables
+std_msgs::Empty emp_msg;	  // variable in the take off and landing class
+geometry_msgs::Twist vel_msg; // variable in the command velocity class
+
+// Variables for Publishing
+ros::Publisher T_pub_empty;		   //take off publisher
+ros::Publisher L_pub_empty;		   //landing publisher
+ros::Publisher velocity_publisher; // velocity publisher
+
+// Variables for Subscribing
+ros::Subscriber pose_subscriber; // ardrone navdata subsciber
+ros::Subscriber gps_subscriber;  // ardrone gps data subsciber
+ros::Subscriber imu_subscriber;  // ardrone imu data subsciber
+ros::Subscriber joy_sub_;
+
+// Variables for Service
+ros::ServiceClient client1; // ardrone camera service
+
+using namespace std;
+
+float lx, ly, lz, ax, ay, az, foc = 685, angx, angy, angz, est_x, est_y, est_z, K_est_x, K_est_y, K_est_z, test_x, test_y, test_z;
+int to, la, f = 200, redX, redY, res = 0, H = 0, I = 0, J = 0;
+
+double k = 0.51, z, tf, ang_time, hem; // k value for tau
+double lat, lon, ele;
+double xD = 7, yD = 4, zD = 1;
+
+//static float pos_x = 0, pos_y = 0, pos_z = 0;
+const double PI = 3.14159265359;
+double Kp = 0.2, Kd = 1, Kpz;						   // Gain for the controller
+double k1 = 2.438, k2 = 0.779, k3 = 1.234, k4 = 0.221; // Gain for the controller
+double tol = 0.4;									   // error tolerance
+int kinect_ready = 0;
+
+//class instances
+ardrone_autonomy::Navdata drone_navdata; // Using this class variable for storing navdata
+sensor_msgs::Imu ang;
+nav_msgs::Odometry odo;
+
+//callback function declarations
+void poseCallback(const ardrone_autonomy::Navdata::ConstPtr &pose_message); // drone actual data
+void imuCallback(const sensor_msgs::Imu::ConstPtr &imu_message);			// drone actual data
+void testdataCallback(const ardrone_autonomy::vector31::ConstPtr &test);
+
+void record();
+int getch();
+
+// basic functions
+void hover(int timee);												   // hovering
+void takeoff();														   // take off
+void land();														   //landing
+void move(float lx, float ly, float lz, float ax, float ay, float az, int moveTime); // publishing command velocity
+
+// Basic Navigation func
+void basic_movement();
+
+int main(int argc, char **argv)
+{
+	//Initiate the ROS
+	ros::init(argc, argv, "waypoint_nav");
+	ros::NodeHandle n; // Nodehandle is like a class where you are operating
+
+	// Publishing the data
+	velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 100); // initialize to send the Control Input
+	T_pub_empty = n.advertise<std_msgs::Empty>("/ardrone/takeoff", 1);		 //initialize to send the take off command  /* Message queue length is just 1 */
+	L_pub_empty = n.advertise<std_msgs::Empty>("/ardrone/land", 1);			 //initialize to send the land command /* Message queue length is just 1 */
+
+	// Subscribing the data
+	pose_subscriber = n.subscribe("/ardrone/navdata", 200, poseCallback); //initialize to receive processed sensor data
+	imu_subscriber = n.subscribe("/ardrone/imu", 200, imuCallback);		  //initialize to receive raw sensor data
+
+	while (ros::ok())
+	{
+		cout << "Press key" << endl;
+		cout << "w-takeoff | s-land | m-basic movements" << endl;
+		cout << "n-record | h-hover for input time" << endl;
+
+		int c = getch(); // call your non-blocking input function
+		int timee;
+		double x, y, z; // for reference value
+
+		switch (c)
+		{
+		case 'm':
+			cout << "\n basic movement menu" << endl;
+			basic_movement();
+		case 'h':
+			cout << "\n enter hover time" << endl;
+			cin >> timee;
+			cout << "\n Hovering in a place" << endl;
+			hover(timee);
+			break;
+		case 's':
+			cout << endl
+				 << "\n Landing initiated" << endl;
+			land();
+			break;
+		case 'w':
+			cout << endl
+				 << "\n Take off initiated" << endl;
+			takeoff();
+			//hover(2);
+			//land();
+			break;
+		case 'n':
+			record();
+			break;
+		default:
+			land();
+			break;
+		}
+		ros::spinOnce(); //if this function is not mentioned the function will stay in the buffer and will not be able to publish
+	}
+	land();
+	cout << "landed";
+}
+
+int getch()
+{
+	static struct termios oldt, newt;
+	tcgetattr(STDIN_FILENO, &oldt); // save old settings
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON);				 // disable buffering
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt); // apply new settings
+
+	int c = getchar(); // read character (non-blocking)
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // restore old settings
+	return c;
+}
+
+void basic_movement()
+{
+	takeoff();
+	// move(0,0,1,0,0,0,1);
+	hover(2);
+	move(1, 0, 0, 0, 0, 0, 2);
+	hover(2);
+	land();
+}
+
+void poseCallback(const ardrone_autonomy::Navdata::ConstPtr &pose_message)
+{
+	drone_navdata.vx = pose_message->vx;
+	drone_navdata.vy = pose_message->vy;
+	drone_navdata.vz = pose_message->vz;
+	drone_navdata.ax = pose_message->ax;
+	drone_navdata.ay = pose_message->ay;
+	drone_navdata.az = pose_message->az;
+	drone_navdata.rotX = pose_message->rotX;
+	drone_navdata.rotY = pose_message->rotY;
+	drone_navdata.rotZ = pose_message->rotZ;
+	drone_navdata.magX = pose_message->magX;
+	drone_navdata.magY = pose_message->magY;
+	drone_navdata.magZ = pose_message->magZ;
+	drone_navdata.altd = pose_message->altd;
+	drone_navdata.tm = pose_message->tm;
+	drone_navdata.header = pose_message->header;
+	drone_navdata.batteryPercent = pose_message->batteryPercent;
+}
+void imuCallback(const sensor_msgs::Imu::ConstPtr &imu_message)
+{
+	ang.header = imu_message->header;
+	ang.angular_velocity = imu_message->angular_velocity;
+	ang.linear_acceleration = imu_message->linear_acceleration;
+}
+
+double deg2rad(double angle_in_degrees)
+{
+	return angle_in_degrees * PI / 180.0;
+}
+
+double getDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2));
+}
+
+/**
+ * TAKE OFF
+ **/
+void takeoff()
+{
+	ros::Rate loop_rate(10);
+	double init_time = ros::Time::now().toSec(); // epoch time
+	double time;
+	while (time < (init_time + 5.0)) /* Send command for five seconds*/
+	{
+		T_pub_empty.publish(emp_msg); /* launches the drone */
+		ros::spinOnce();			  // feedback
+		loop_rate.sleep();
+		time = ros::Time::now().toSec();
+	} //time loop
+	ROS_INFO("ARdrone launched");
+	hover(2);
+}
+
+/**
+ * LAND
+ **/
+void land()
+{
+	ros::Rate loop_rate(10);
+	while (ros::ok())
+	{
+		double init_time = ros::Time::now().toSec();
+		double time;
+		while (time < (init_time + 5.0)) /* Send command for five seconds*/
+		{
+			L_pub_empty.publish(emp_msg); /* lands the drone */
+			ros::spinOnce();
+			loop_rate.sleep();
+			time = ros::Time::now().toSec();
+		} //time loop
+		ROS_INFO("ARdrone landed");
+		exit(0);
+	} //ros::ok loop
+}
+
+/**
+ * HOVER
+ **/
+void hover(int timee)
+{
+	double t0 = ros::Time::now().toSec(); //ros has a 'Time' function in which the current time can be found by using 'now'.                                         							we need time in sec to compute hence 'tosec'
+	double t1;
+
+	ros::Rate loop_rate(200);
+
+	do
+	{
+		t1 = ros::Time::now().toSec(); //ros has a 'Time' function in which the current time can be found by using 'now'.                                         							we need time in sec to compute hence 'tosec'
+
+		vel_msg.linear.x = 0;
+		vel_msg.linear.y = 0;
+		vel_msg.linear.z = 0;
+
+		vel_msg.angular.x = 0;
+		vel_msg.angular.y = 0;
+		vel_msg.angular.z = 0;
+
+		velocity_publisher.publish(vel_msg);
+
+		ros::spinOnce(); //if this function is not mentioned the function will stay in the buffer and cannot be able to publish
+		loop_rate.sleep();
+	} while (t1 <= (t0 + timee));
+}
+
+/**
+ * MOVE TO POINT
+ **/
+void move(float lx, float ly, float lz, float ax, float ay, float az, int moveTime)
+{
+	//ros::Rate loop_rate(10);
+	//ros::spinOnce(); //if this function is not mentioned the function will stay in the buffer and cannot be able to publish
+	//loop_rate.sleep();
+	double t0 = ros::Time::now().toSec(); //ros has a 'Time' function in which the current time can be found by using 'now'.                                         							we need time in sec to compute hence 'tosec'
+	double t1;
+	ros::Rate loop_rate(10);
+	do
+	{
+		t1 = ros::Time::now().toSec(); //ros has a 'Time' function in which the current time can be found by using 'now'.                                         							we need time in sec to compute hence 'tosec'
+		//defining the linear velocity
+		vel_msg.linear.x = lx;
+		vel_msg.linear.y = ly;
+		vel_msg.linear.z = lz;
+
+		//defining the linear velocity
+		vel_msg.angular.x = ax;
+		vel_msg.angular.y = ay;
+		vel_msg.angular.z = az;
+
+		velocity_publisher.publish(vel_msg);
+
+		ros::spinOnce(); //if this function is not mentioned the function will stay in the buffer and cannot be able to publish
+		loop_rate.sleep();
+
+	} while (t1 <= (t0 + 0.5));
+}
+
+void record()
+{
+	double t0 = ros::Time::now().toSec();
+	double i = 0, f = 200.0, d2r = 3.14159265 / 180, dt;
+	ofstream myfile;
+	myfile.open("/home/icgel/icgel7/recorded_data.txt", ios::out | ios::app);
+	myfile << "gx gy gz ax ay az vxo vyo vzo theta phi psi lat lon ele T" << endl;
+	ros::Rate loop_rate(200);
+	do
+	{ //if(i<=500)
+		//move(0.1, 0, 0, 0, 0, 0);
+		/*else if (i>500 && i<1000){
+				move(0,0,0,0,0,0);}
+			else{
+				move(0.1,0,0,0,0,0);}
+			*/
+		myfile << ang.angular_velocity.x << setw(20);
+		myfile << ang.angular_velocity.y << setw(20);
+		myfile << ang.angular_velocity.z << setw(20);
+
+		//myfile<<drone_navdata.ax*9.81<<setw(20);
+		//myfile<<drone_navdata.ay*9.81<<setw(20);
+		//myfile<<drone_navdata.az*9.81<<setw(20);
+		myfile << ang.linear_acceleration.x << setw(20);
+		myfile << ang.linear_acceleration.y << setw(20);
+		myfile << ang.linear_acceleration.z << setw(20);
+
+		myfile << drone_navdata.vx * 0.001 << setw(20);
+		myfile << drone_navdata.vy * 0.001 << setw(20);
+		myfile << drone_navdata.vz * 0.001 << setw(20);
+
+		myfile << (drone_navdata.rotY) * d2r << setw(20); //theta-pitch
+		myfile << (drone_navdata.rotX) * d2r << setw(20); //phi-roll
+		myfile << (drone_navdata.rotZ) * d2r << setw(20);
+
+		myfile << setprecision(12) << lat << setw(20);
+		myfile << setprecision(12) << lon << setw(20);
+		myfile << setprecision(12) << ele << setw(20);
+
+		double t1 = ros::Time::now().toSec();
+		dt = t1 - t0;
+		myfile << dt << endl;
+
+		i = i + 1;
+
+		ros::spinOnce(); //if this function is not mentioned the function will stay in the buffer and cannot be able to publish
+		loop_rate.sleep();
+	} while (i < 10000);
+	myfile.close();
+	land();
+}
